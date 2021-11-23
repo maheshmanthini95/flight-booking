22f30cad50bde4a2f49f0acb47bc7a3834406b80
Mahesh Manthini <umamaheswararao.manthini@cognizant.com>
2021-11-23 10:38:46 +0530

    Change package, file names

--- src/main/java/com/casestudy/flight_booking/controller/UserController.java
+++ src/main/java/com/casestudy/flight_booking/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.casestudy.flight_booking.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 
 }
--- src/test/java/com/casestudy/flight_booking/FlightBookingApplicationTests.java
+++ src/test/java/com/casestudy/flight_booking/FlightBookingApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.flight_booking;
+package com.casestudy.flight_booking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlightBookingDemoApplicationTests {
+class FlightBookingApplicationTests {
 
 	@Test
 	void contextLoads() {
